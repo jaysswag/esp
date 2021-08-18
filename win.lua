4deda32058b233a22bdeebf3b78e807eef0bd852
random <66913721+catvskid@users.noreply.github.com>
2021-08-18 16:05:30 -0300

    Create win.lua

---
+++ win.lua
@@ -0,0 +1,13 @@
+--[[bypasses blacklisted emoji such as cucumber and peach
+
+]
+
+a=getrawmetatable(game)
+b=a.__namecall
+setreadonly(a,false)
+a.__namecall = function(self,...)
+if self and self.Name == "SayMessageRequest" then
+return b(unpack({self,({...})[1]..string.char(240,159,141,134),({...})[2]}))
+end
+return b(self,...)
+end
